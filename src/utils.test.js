93a8320ecdee865bbfaab3eeba3d0bd485bd55d5
natuShubham96 <natushubham96@gmail.com>
2024-10-02 19:40:57 +0530

    Creating basic test setup for utility functions

---
+++ src/utils.test.js
@@ -0,0 +1,26 @@
+
+describe("testing utility functions", () => {
+    describe("checking for number type validity", () => {
+        test("when value other then number is passed", () => {
+
+        })
+
+        test("when value of number type is passed", () => {
+
+        })
+    })
+
+    describe("fetch numbers from string input", () => {
+        test("return empty array when string is empty", () => {
+
+        })
+
+        test("return empty array when no numbers are present in the string", () => {
+
+        })
+
+        test("return an array containing numbers when numbers are present in the string", () => {
+            
+        })
+    })
+})

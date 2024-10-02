d3be5fda3b9f56b6d7e0dc573e2cf2c3f20d33c3
natuShubham96 <natushubham96@gmail.com>
2024-10-02 22:35:52 +0530

    Adding styled-components for styling

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
-import { useState } from 'react';
-import './App.css';
-import { add } from './utils';
+import { useState } from "react";
+import "./App.css";
+import { add } from "./utils";
+import styled from "styled-components";
 
 function App() {
-  const [str, setStr] = useState('');
+  const [str, setStr] = useState("");
   const [result, setResult] = useState(0);
 
- const onChangeString = (event) => {
-    if(event?.target?.value) {
+  const onChangeString = (event) => {
+    if (event?.target?.value) {
       setStr(event?.target?.value);
     }
-  }
+  };
 
   const onCalculation = () => {
     const sum = add(str);
     setResult(sum);
-    setStr('');
-  }
+    setStr("");
+  };
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%', gap: '10px'}}>
-    <div style={{display: 'flex', gap: '10px'}}>
-      <input placeholder='Enter alphanumeric string' value={str} onChange={(e) => onChangeString(e)}/>
-      <button onClick={() => str && onCalculation()}>Trigger Calculation</button>
-    </div>
-    <span>Result: {result}</span>
-    </div>
+    <Container>
+      <ChildContainer>
+        <input
+          placeholder="Enter alphanumeric string"
+          value={str}
+          onChange={(e) => onChangeString(e)}
+        />
+        <button onClick={() => str && onCalculation()}>
+          Trigger Calculation
+        </button>
+      </ChildContainer>
+      <span>Result: {result}</span>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  gap: 10px;
+`;
+
+const ChildContainer = styled.div`
+  display: flex;
+  gap: 10px;
+`;
 
 export default App;
